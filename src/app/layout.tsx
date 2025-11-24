ac21ec6f7d31d77f5abd9b25b04411079e5e6b20
Ellsy23 <dlwnsgus3153@naver.com>
2025-11-24 16:58:22 +0900

    feat: add 3D background with particles and fade-in animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Background3D } from "@/components/Background3D";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-background font-sans antialiased`}
       >
+        <Background3D />
         <div className="relative flex min-h-screen flex-col">
           <Header />
           <div className="flex-1">{children}</div>
